9eecc0edbd7e29b343ad5c146162a0ccffb55a44
codedineshgupta <code.dineshgupta@gmail.com>
2023-07-24 02:53:33 +0530

    Added test case for Alarm class

--- TirePressureMonitoringSystem.Test/AlarmTest.cs
+++ TirePressureMonitoringSystem.Test/AlarmTest.cs
@@ -14,13 +14,9 @@ namespace RefactorMircoExcercise.TirePressureMonitoringSystem
         public void Check_WhenPressure_is_Below_LowThreshold_Should_SetAlarmOn()
         {
             // Arrange
-            var mockSensor = new Mock<Sensor>();
+            var mockSensor = new Mock<ISensor>();
             mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(15); // Below LowPressureThreshold
-            var alarm = new Alarm();
-
-            // Use reflection to set the private field "_sensor" to the mocked sensor
-            var sensorField = alarm.GetType().GetField("_sensor", BindingFlags.NonPublic | BindingFlags.Instance);
-            sensorField.SetValue(alarm, mockSensor.Object);
+            var alarm = new Alarm(mockSensor.Object);
 
             // Act
             alarm.Check();
@@ -28,6 +24,67 @@ namespace RefactorMircoExcercise.TirePressureMonitoringSystem
             // Assert
             Assert.True(alarm.AlarmOn);
         }
-        
+
+        [Fact]
+        public void Check_WhenPressure_is_Above_HighThreshold_Should_SetAlarmOn()
+        {
+            // Arrange
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(22); // Above HighPressureThreshold
+            var alarm = new Alarm(mockSensor.Object);
+
+            // Act
+            alarm.Check();
+
+            // Assert
+            Assert.True(alarm.AlarmOn);
+        }
+
+        [Fact]
+        public void Check_WhenPressure_is_WithinThreshold_Should_Not_Set_AlarmOn()
+        {
+            // Arrange
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(18); // Within threshold
+            var alarm = new Alarm(mockSensor.Object);
+
+            // Act
+            alarm.Check();
+
+            // Assert
+            Assert.False(alarm.AlarmOn);
+        }
+
+        [Fact]
+        public void Check_WhenPressure_is_ExactlyAtLowThreshold_ShouldNot_Set_AlarmOn()
+        {
+            // Arrange
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(17); // At LowPressureThreshold
+            var alarm = new Alarm(mockSensor.Object);
+
+            // Act
+            alarm.Check();
+
+            // Assert
+            Assert.False(alarm.AlarmOn);
+        }
+
+        [Fact]
+        public void Check_WhenPressure_is_ExactlyAtHighThreshold_Should_Not_SetAlarmOn()
+        {
+            // Arrange
+            var mockSensor = new Mock<ISensor>();
+            mockSensor.Setup(x => x.PopNextPressurePsiValue()).Returns(21); // At HighPressureThreshold
+            var alarm = new Alarm(mockSensor.Object);
+
+            // Act
+            alarm.Check();
+
+            // Assert
+            Assert.False(alarm.AlarmOn);
+        }
+
+
     }
 }
